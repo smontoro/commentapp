64031bc9dceb4bab1b5735469cc3bddff4b25d2c
smontoro <sarahjmontoro@gmail.com>
2017-10-14 16:13:05 -0700

    added style

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
-//Exersize: Create a new div elemnent when the page is created
-
-/*var newDiv = document.createElement('div')
-var textHere = document.createTextNode('write our text here')
-newDiv.appendChild(textHere)
-var addHere = document.getElementById('addHere')
-addHere.appendChild(newDiv)*/
-
-/*var newh1 = document.createElement('h1')
-var newImg = document.createElement('img')
-newh1.className ='addedClass' 
-var textHere= document.createTextNode('wazzzzzupppp')
-newh1.appendChild(textHere)
-newImg.setAttribute('src', 'https://c1.staticflickr.com/9/8115/8673546462_73b2df8cb3_b.jpg')
-var addHere = document.getElementById('addHere')
-addHere.appendChild(newh1)
-addHere.appendChild(newImg)*/
-
 function postComment() {
+
+	//gets the date of the post
+	var date = new Date()
+	//stores the date in the text node
+	dateText = document.createTextNode(date)
+	//creates a new h4 element
+	var pDate = document.createElement("p")
+	//puts the date text in the element
+	pDate.appendChild(dateText)
+	//adds the date to the .commentList div
+	document.getElementById('commentList').appendChild(pDate)
+
 	//stores the input comment in the "comment" var
 	var comment = document.getElementById('commentInput').value
 	//stores the input that's stored in the "comment" var in the "text" var
 	var text = document.createTextNode(comment)
 	//creates a new p element to store the text in
-	var newP = document.createElement("p")
+	var articleTitle = document.createElement("h2")
 	//stores the text in the p element
-	newP.appendChild(text)
+	articleTitle.appendChild(text)
 	//adds the new p element with its text to the .commentList div
-	document.getElementById('commentList').appendChild(newP)
+	document.getElementById('commentList').appendChild(articleTitle)
+
+
+	//stores the input link in the "link" var
+	var link = document.getElementById("linkInput").value
+	//creates a link element
+	var a = document.createElement("a")
+	//adds the inputed link to the link element
+	a.setAttribute("href", link)
+
 
 
 	//stores the input URL in the "image" var
@@ -35,6 +38,28 @@ function postComment() {
 	var newImg = document.createElement("img")
 	//attached the var image input URL to the new img tag
 	newImg.setAttribute("src", image)
-	//adds the new image to the comment div
-	document.getElementById("commentList").appendChild(newImg)
+	//adds style to the image
+	newImg.className= "input-image"
+	//adds the img tag inside of the link tag
+	a.appendChild(newImg)
+	//adds the new linked image to the comment div
+	document.getElementById("commentList").appendChild(a)
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
